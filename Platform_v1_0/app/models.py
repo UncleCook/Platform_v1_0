15f53ba1a4b893ef4003fddad7565a6b61d71cec
Thomas Osborn <tom.osborn@outlook.com>
2024-02-02 10:52:37 +0000

    Add project files.

---
+++ Platform_v1_0/app/models.py
@@ -0,0 +1,18 @@
+"""
+Definition of models.
+"""
+
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model): 
+   name = models.CharField(max_length=50) 
+   city = models.CharField(max_length=50) 
+   state = models.CharField(max_length=2) 
+   create_date = models.DateTimeField() 
+   phone_number = models.CharField(max_length=20) 
+   email = models.CharField(max_length=20) 
+
+   def __str__(self): 
+      return self.name 
+  
